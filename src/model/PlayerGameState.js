e64b4139061e08f0fde386653bd51373bdc6e717
Branko <branko.trnavac@programerika.com>
2022-08-11 16:17:54 +0200

    added comment

--- src/model/PlayerGameState.js
+++ src/model/PlayerGameState.js
@@ -4,7 +4,7 @@ export default class PlayerGameState {
     this.cols = 10;
     this.rows = 10;
     this.isFinished = false;
-    this.score = 0;
+    this.score = 0; // da li da krenemo od 100 kako bi izbegli da setujemo na 100 u gameEndSuccessfully
     this.minefield = [];
     this.startTime = Date.now();
   }
