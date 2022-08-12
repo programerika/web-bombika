7e59a5640aa49670e3e86ee6e1c634ca3a88a0fd
Marko <marko.mitrovic@programerika.com>
2022-08-12 13:39:58 +0200

    Added GameTableComponent and CellComponent

---
+++ src/components/GameTableComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><CellComponent /></div>
+</template>
+<script>
+import CellComponent from "./cellComponent.vue";
+export default {
+  setup() {},
+  components: { CellComponent },
+};
+</script>
