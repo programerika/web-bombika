cef2a26a242c2714e37847d2f0cab4ac2a45fb86
Marko <marko.mitrovic@programerika.com>
2022-08-25 15:19:24 +0200

    Fixed game score

--- src/model/gameState.js
+++ src/model/gameState.js
@@ -1,11 +1,11 @@
 export default class GameState {
   constructor() {
-    this.numberOfBombs = 10;
+    this.numberOfBombs = 1;
     this.cols = 10;
     this.rows = 10;
     this.isFinished = false;
     this.score = 0;
-    this.maxScore = 100;
+    this.maxScore = 105;
     this.minefield = [];
     this.startTime = Date.now();
   }
