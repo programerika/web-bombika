f2b4b1126280175bb94bd6b4836792296cfc99a3
Marko <marko.mitrovic@programerika.com>
2022-09-01 09:34:47 +0200

    Help component - Icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,9 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+export default createVuetify({
+  icons: {
+    defaultSet: "mdi", // This is already the default value - only for display purposes
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
