25b7ec0eb318bd846e262dc0d7b56444b99726a6
MarkoMitrovic00 <marko.mitrovic@programerika.com>
2022-09-23 11:04:28 +0200

    Added 2 more functions in scoreBoardViewModel and re-enabled prettier

--- src/services/ErrorNotificationService.js
+++ src/services/ErrorNotificationService.js
@@ -1,13 +1,13 @@
 export default function notifyError(
-    error,
-    alertUser = false,
-    userMessage,
-    appendReason = false
-  ) {
-    if (alertUser) {
-      const finalMessage =
-        userMessage + (appendReason ? " Reason: " + error.message : "");
-      window.alert(finalMessage);
-    }
-    console.error(error);
+  error,
+  alertUser = false,
+  userMessage,
+  appendReason = false
+) {
+  if (alertUser) {
+    const finalMessage =
+      userMessage + (appendReason ? " Reason: " + error.message : "");
+    window.alert(finalMessage);
   }
+  console.error(error);
+}
