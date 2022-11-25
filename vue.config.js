aa9214056c23ea6a786d59ee6301e74f36a2983e
MarkoMitrovic00 <marko.mitrovic@programerika.com>
2022-11-25 10:01:21 +0100

    Added postcss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const _ = require("lodash");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: { extract: false },
@@ -19,4 +20,35 @@ module.exports = defineConfig({
       libraryExport: "default",
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule("css")
+      .oneOf("normal")
+      .use("postcss-loader")
+      .tap((options) => {
+        _.merge(options, postcssRemoveGlobalStyle);
+        console.log(options);
+        return options;
+      });
+  },
 });
+
+const postcssRemoveGlobalStyle = {
+  postcssOptions: {
+    plugins: [
+      [
+        "postcss-prefix-selector",
+        {
+          prefix: ".bombikaGlobalCss",
+          transform(prefix, selector, prefixedSelector, filePath, rule) {
+            if (selector.match(/^([*])/)) {
+              return prefixedSelector;
+            }
+
+            return selector;
+          },
+        },
+      ],
+    ],
+  },
+};
